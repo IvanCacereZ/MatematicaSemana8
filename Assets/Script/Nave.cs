e82877fecdddec02a36205d182d1d70b15b961e2
IvanCacereZ <130480223+IvanCacereZ@users.noreply.github.com>
2023-05-14 11:33:12 -0500

    1.1
    Se actualizo la nave para que esta pueda rotar dependiendo la dirección a donde se mueva

--- Assets/Script/Nave.cs
+++ Assets/Script/Nave.cs
@@ -6,39 +6,56 @@ public class Nave : MonoBehaviour
 {
     [SerializeField] private Rigidbody myRB;
     [SerializeField] private float velocityModifier = 3f;
-    [SerializeField] Vector3 angulos;
-    [SerializeField] Quaternion qx = Quaternion.identity;
-    [SerializeField] Quaternion qy = Quaternion.identity;
-    [SerializeField] Quaternion qz = Quaternion.identity;
-
-    [SerializeField] Quaternion r = Quaternion.identity;
+    public Vector3 angulos;
+    
     float anguloSen, anguloCos;
     void Update()
     {
         float GetHorizontal = Input.GetAxis("Horizontal");
         float GetVertical = Input.GetAxis("Vertical");
         Vector2 movementPlayer = new Vector2(GetHorizontal, GetVertical);
+        if (Input.GetKey("space"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1 * Time.deltaTime);
+        }
+        if(GetHorizontal > 0)
+        {
+            angulos.z = -55f;
+        }
+        else if (GetHorizontal < 0)
+        {
+            angulos.z = 55f;
+        }
+        else
+        {
+            angulos.z = 0f;
+        }
+        if (GetVertical > 0)
+        {
+            angulos.x = 30f;
+        }
+        else if (GetVertical < 0)
+        {
+            angulos.x = -30f;
+        }
+        else
+        {
+            angulos.x = 90;
+        }
         myRB.velocity = movementPlayer * velocityModifier;
-        /*if(GetHorizontal == 0 && GetVertical == 0){
-            angulos = new Vector3(90,0,0);
-        }
-        else if(GetHorizontal < 0){
-            angulos = new Vector3(90,-20,0);
-        }
-        else if(GetHorizontal > 0){
-            angulos = new Vector3(90,20,0);
-        }
-        else if(GetVertical < 0){
-            angulos = new Vector3(65,0,0);
-        }
-        else if(GetVertical > 0){
-            angulos = new Vector3(115,0,0);
-        }*/
+        GetRotation();
     }
-    private void FixedUpdate()
+    private void GetRotation()
     {
+        Quaternion qx = Quaternion.identity;
+        Quaternion qy = Quaternion.identity;
+        Quaternion qz = Quaternion.identity;
+        Quaternion r = Quaternion.identity;
+        anguloSen = Mathf.Sin(Mathf.Deg2Rad * angulos.z * 0.5f);
+        anguloCos = Mathf.Cos(Mathf.Deg2Rad * angulos.z * 0.5f);
+        qz.Set(0, 0, anguloSen, anguloCos);
 
-        /*anguloSen = Mathf.Sin(Mathf.Deg2Rad * angulos.x * 0.5f);
+        anguloSen = Mathf.Sin(Mathf.Deg2Rad * angulos.x * 0.5f);
         anguloCos = Mathf.Cos(Mathf.Deg2Rad * angulos.x * 0.5f);
         qx.Set(anguloSen, 0, 0, anguloCos);
 
@@ -46,11 +63,7 @@ public class Nave : MonoBehaviour
         anguloCos = Mathf.Cos(Mathf.Deg2Rad * angulos.y * 0.5f);
         qy.Set(0, anguloSen, 0, anguloCos);
 
-        anguloSen = Mathf.Sin(Mathf.Deg2Rad * angulos.z * 0.5f);
-        anguloCos = Mathf.Cos(Mathf.Deg2Rad * angulos.z * 0.5f);
-        qz.Set(0, 0, anguloSen, anguloCos);
-
         r = qy * qx * qz;
-        transform.rotation = r;*/
+        transform.rotation = r;
     }
 }
